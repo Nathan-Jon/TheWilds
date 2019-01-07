9bbdb78d6846b79268a39039427b74126e2bcc20
Nathan Robertson <nathan_robertson@hotmail.co.uk>
2019-01-07 21:25:58 +0000

    Menus Update
    Added opening menu and end game menu
    
    No way to access the end game ui currently
    
    The Ending title screen needs a button saying exit game

---
+++ The Wilds/Assets/ButtonHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+
+
+	/// <summary>
+    /// Change the scene to the assigned value
+    /// </summary>
+    /// <param name="sceneNum"></param>
+	public void ChangeScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+
+    /// <summary>
+    /// Closes the application 
+    /// </summary>
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+}
